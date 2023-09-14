5207262517ad8856905c8e94a991c36de6f1b977
Albert Prince Mensah <apmensah@gmail.com>
2023-09-14 16:26:33 +0000

    added Room model

--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+rooms = [
+    {'id' : 1, 'name': 'lets learn python'},
+    {'id' : 2, 'name': 'lets learn java'},
+    {'id' : 3, 'name': 'lets learn C++'}
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for item in rooms:
+        if item['id'] == pk:
+            room = item
+        context = {'room': room}
+    return render(request, 'base/room.html', context)
