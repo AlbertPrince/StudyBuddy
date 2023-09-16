a5dc3bcf9d1228b89c27223e1cdf10ab32ac5fb1
Albert Prince Mensah <apmensah@gmail.com>
2023-09-16 22:13:45 +0000

    added delete view and updated view

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<int:pk>/', views.room, name="room"),
-    path('create-room/', views.createRoom, name="createRoom")
+    path('create-room/', views.createRoom, name="createRoom"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
 ]
