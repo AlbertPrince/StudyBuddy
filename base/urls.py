22ad9e3bf2eb4f129280e288511f0b3fa70c619c
Albert Prince Mensah <apmensah@gmail.com>
2023-09-20 12:51:13 +0000

    fixed login and logout views

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('', views.home, name="home"),
     path('room/<int:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="createRoom"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
-    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    
 ]
--- base/views.py
+++ base/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpResponse, Http404
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
-# rooms = [
-#     {'id' : 1, 'name': 'lets learn python'},
-#     {'id' : 2, 'name': 'lets learn java'},
-#     {'id' : 3, 'name': 'lets learn C++'}
-# ]
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)                        
+                                )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    
+    context = {"rooms": rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -20,6 +29,7 @@ def room(request, pk):
     context = {'room' : room}
     return render(request, 'base/room.html', context)
 
+@login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -30,9 +40,13 @@ def createRoom(request):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='/login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
     
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -43,10 +57,61 @@ def updateRoom(request, pk):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
+@login_required(login_url='/login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+    
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+# def loginPage(request):
+    
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+        
+    #     try:
+    #         user = User.objects.get(username=username)
+    #     except:
+    #         messages.error(request, 'User does not exist')
+        
+    #     user = authenticate(request, username=username, password=password)
+        
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect('home')
+    #     else:
+    #         messages.error(request, 'username OR password does not exist')
+    
+    # context = {}
+    # return render(request, 'base/login_register.html', context)
+
+def loginPage(request):
+    page = 'login'
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username OR password does not exist')
+    
+    return render(request, "base/login_register.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
     
